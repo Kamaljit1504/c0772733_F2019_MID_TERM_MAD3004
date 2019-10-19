d1df878f3e65f0e4bfd66d290e5dd208897ef956
Kamaljit1504 <kaurkamaljit963@gmail.com>
2019-10-19 18:17:33 -0400

    cmmit

---
+++ c0772733_F2019_MID_TERM_MAD3004/usedData.swift
@@ -0,0 +1,23 @@
+//
+//  usedData.swift
+//  c0772733_F2019_MID_TERM_MAD3004
+//
+//  Created by MacStudent on 2019-10-19.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+extension Double
+    
+{
+    func gb_used()->String
+    {
+        return "\(self) gb"
+    }
+    
+    func dataUnit()->String
+    {
+        return "\(self) Units"
+    }
+    
+}
