77cda5e433c4b5df476ec043e44d7b6c2d56de35
Kamaljit1504 <kaurkamaljit963@gmail.com>
2019-10-19 18:19:02 -0400

    cmit

--- c0772733_F2019_MID_TERM_MAD3004/Date.swift
+++ c0772733_F2019_MID_TERM_MAD3004/Date.swift
@@ -9,14 +9,15 @@
 import Foundation
 extension Date
 {
-    
     func dateformatter() -> String {
         let dateFormatterPrint=DateFormatter()
         dateFormatterPrint.dateFormat="EEEE, dd MM, yyyy"
         let formattedDate = dateFormatterPrint.string(from: self)
         return formattedDate
-        
+    
     }
+        
+    
     
     
 }
